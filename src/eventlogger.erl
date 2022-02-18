4b541ddaf15466c05e1e797172b34270032c7d18
Yoko OYAMA <yowcow@gmail.com>
2022-02-18 09:47:13 +0900

    format

--- src/eventlogger.erl
+++ src/eventlogger.erl
@@ -1,6 +1,10 @@
 -module(eventlogger).
 
--export([log/2]).
+-export([format/3, log/2]).
+
+-spec format(Event :: atom(), Format :: string(), Args :: [term()]) -> ok.
+format(Event, Format, Args) ->
+    log(Event, io_lib:format(Format, Args)).
 
 -spec log(Event :: atom(), Output :: binary()) -> ok.
 log(Event, Output) when is_list(Output) ->
--- test/eventlogger_tests.erl
+++ test/eventlogger_tests.erl
@@ -2,12 +2,58 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-log_test_() ->
+format_test_() ->
     {setup,
      fun() ->
         {ok, TmpDir} = eventlogger_util:tmpdir(["/tmp/", ?MODULE]),
         {ok, Pid} = gen_event:start_link({local, eventlogger_manager}),
         LogFile = [TmpDir, "/test1.log"],
+        ok =
+            gen_event:add_handler(Pid,
+                                  {eventlogger_file_writer, 1},
+                                  [{event, foo},
+                                   {file, LogFile},
+                                   {modes, [append, raw, write]},
+                                   {maxbytes, 1000},
+                                   {count, 2}]),
+        [{tmpdir, TmpDir}, {logfile, LogFile}, {gen_event, Pid}]
+     end,
+     fun(Args) ->
+             Pid = proplists:get_value(gen_event, Args),
+             gen_event:stop(Pid)
+     end,
+     fun(Args) ->
+             LogFile = proplists:get_value(logfile, Args),
+             Cases = [
+                      {"format numbers",
+                       "numbers: ~.2b, ~.10b, ~.16b",
+                       [15, 15, 15],
+                       <<"numbers: 1111, 15, f\n">>
+                      },
+                      {"format characters",
+                       "characters: ~s ~ts",
+                       ["AAA", unicode:characters_to_list("あああ")],
+                       <<
+                         "numbers: 1111, 15, f\n",
+                         "characters: AAA あああ\n"/utf8
+                       >>
+                      }
+                     ],
+             F = fun({Title, Format, Input, Expected}) ->
+                         ok = eventlogger:format(foo, Format, Input),
+                         FileData = file:read_file(LogFile),
+                         [{Title, ?_assertEqual({ok, Expected}, FileData)}]
+                 end,
+             lists:map(F, Cases)
+     end}.
+
+
+log_test_() ->
+    {setup,
+     fun() ->
+        {ok, TmpDir} = eventlogger_util:tmpdir(["/tmp/", ?MODULE]),
+        {ok, Pid} = gen_event:start_link({local, eventlogger_manager}),
+        LogFile = [TmpDir, "/test2.log"],
         ok =
             gen_event:add_handler(Pid,
                                   {eventlogger_file_writer, 1},
